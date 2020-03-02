e51018ec533c5761aa2fc27532be08ac73e9027d
dartrhevan <vovasatunkin@mail.ru>
2020-03-02 14:47:13 +0500

    interpolate function - first version

--- interpolate.cpp
+++ interpolate.cpp
@@ -1,3 +1,11 @@
 #include "interpolate.h"
 
 
+
+ResultFunction interpolate(const std::vector<Point>& points)
+{
+    ResultFunction res;
+    for(int i = 1; i < points.size(); ++i)
+        res.addSplain(ElementaryFunction(points[i - 1], points[i]));
+    return res;
+}
